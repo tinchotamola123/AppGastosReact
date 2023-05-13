4fdc479270bb42234d4d3e39211523ad5a76201f
Martin Tamola <tinchotamola123@gmail.com>
2023-05-13 14:41:07 -0300

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,27 @@ import TransactionList from './components/transactions/TransactionList'
 import IncomeExpenses from './components/IncomeExpenses'
 import ExpenseChart from './components/transactions/ExpenseChart'
 
+
+var mediaquery = window.matchMedia("(max-width: 400px)");
+function funcion() {
+  var div = document.querySelector(".grande");
+  if (mediaquery.matches) {
+    div.classList.remove("rojo");
+    div.removeAttribute("id");
+  } else {
+    div.classList.add("rojo");
+    div.setAttribute("id", "blanco")
+  }
+}
+mediaquery.addListener(funcion);
+
+
 function App() {
   return (
     <GlobalProvider>
       <div className='bg-zinc-900 text-white min-h-screen	flex justify-center items-center'> 
-        <div className='container mx-auto w-3/6'>
-          <div className='bg-zinc-800 p-10 rounded-lg flex gap-x-2'>
+        <div id='contenedor1' className='container mx-auto w-3/6'>
+          <div id='contenedor' className='bg-zinc-800 p-10 rounded-lg flex gap-x-2'>
             <div className=''>
               <h1 className='text-4xl	 font-bold'>Gastos</h1>
               <IncomeExpenses/>
